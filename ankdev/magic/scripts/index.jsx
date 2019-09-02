9f34452c63157666be0d3fdd2d876f279e9027fc
Ankit Kumar <ankits.kvmdam@hotmail.com>
2019-09-02 23:26:05 +0530

    landing page create

--- ankdev/magic/scripts/index.jsx
+++ ankdev/magic/scripts/index.jsx
@@ -1,3 +1,30 @@
 import "../stylesheet/main.scss"
 
-console.log("Hello World")
+const light = document.getElementById("light")
+const dark = document.getElementById("dark")
+const body = document.getElementsByTagName("BODY")[0]
+
+function changeTheme(event){
+    light.classList.remove('active')
+    dark.classList.remove('active')
+    body.classList.remove("theme-dark", "theme-light")
+    
+    switch(event.currentTarget.id){
+        case "light": 
+            light.classList.add('active')
+            body.classList.add("theme-light")
+            break
+        default:
+            dark.classList.add("active")
+            body.classList.add("theme-dark")
+    }
+}
+
+function init(){
+    body.classList.add("theme-dark")
+
+    light.addEventListener('click', changeTheme)
+    dark.addEventListener('click', changeTheme)
+}
+
+init()
