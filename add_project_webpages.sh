702e235328337d959ae058c01583e2b1d0772e1f
Ankit Kumar <hello@ankdev.me>
2019-12-07 21:14:44 +0530

    add script to add project websites

---
+++ add_project_webpages.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+# move to public folder
+
+# clean-jsdoc-theme
+#-------------------
+
+## first remove the exisiting copy
+echo Removing the exising copy of clean-jsdoc-theme...
+rm -rf ankdev/public/clean-jsdoc-theme
+## echo Cloning the new copy of clean-jsdoc-theme...
+git clone --depth 1 --single-branch -b production https://github.com/ankitskvmdam/clean-jsdoc-theme.git ./ankdev/public/clean-jsdoc-theme
+## echo Removing unnecessary files...
+cd ./ankdev/public/clean-jsdoc-theme && git filter-branch --subdirectory-filter demo/output
+echo Removing git files of clean-jsdoc-theme
+rm -rf ./ankdev/public/clean-jsdoc-theme/.git
+#---------END--------
