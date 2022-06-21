dffb1f5aebb5aeb126935680eae71ce348ff1791
Ankit Kumar <ankits.kvmdam@hotmail.com>
2022-06-21 16:20:14 +0530

    small fixes

--- watch-dev.js
+++ watch-dev.js
@@ -1,16 +1,16 @@
-const path = require('path')
-const fse = require('fs-extra')
-const { exec } = require('child_process')
+const path = require("path")
+const fse = require("fs-extra")
+const { exec } = require("child_process")
 
-const rollup = exec('npm run rollup:dev')
-const hugo = exec('npm run hugo')
+const rollup = exec("npm run rollup:dev")
+const hugo = exec("npm run hugo")
 
-const assets = path.join(__dirname, 'assets')
-const staticDir = path.join(__dirname, 'static')
+const assets = path.join(__dirname, "assets")
+const staticDir = path.join(__dirname, "static")
 
-if(fse.existsSync(assets)){
+if (fse.existsSync(assets)) {
     fse.copySync(assets, staticDir)
 }
 
-hugo.stdout.on('data', (data) => console.log(data))
-rollup.stdout.on('data', (data) => console.log(data))
+hugo.stdout.on("data", (data) => console.log(data))
+rollup.stdout.on("data", (data) => console.log(data))
