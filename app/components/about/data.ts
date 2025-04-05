c1693d1b9e398fbdb5285a917f3c00a847bdc66f
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-04-06 01:10:32 +0900

    save data

--- app/components/about/data.ts
+++ app/components/about/data.ts
@@ -7,12 +7,62 @@ export type TTimelineData = {
 export const timelineData: TTimelineData[] = [
   {
     id: "landed-on-planet-earth",
-    timestamp: 865942200,
-    title: "Landed on planet earth",
+    timestamp: 865942200, // 1997
+    title: "Landed on planet earth.",
   },
   {
     id: "first-computer-game",
-    timestamp: 1055244600,
+    timestamp: 1086867000, // 2004
     title: "Played first computer game.",
+  },
+  {
+    id: "learn-html-css",
+    timestamp: 1213097400, // 2008
+    title: "Learn HTML and CSS.",
+  },
+  {
+    id: "build-my-computer-game",
+    timestamp: 1339327800, // 2012
+    title: "Build first computer game using unity engine.",
+  },
+  {
+    id: "terminal-based-spreadsheet",
+    timestamp: 1465558200, // 2016
+    title: "Built terminal based spreadsheet using C++.",
+  },
+  {
+    id: "co-founded-asaan",
+    timestamp: 1488850200, // March 7, 2017
+    title: "Co-founded Asaan Contracts Private Limited.",
+  },
+  {
+    id: "build-first-website-using-react",
+    timestamp: 1528637400, //2018
+    title: "Built website for Technical fest.",
+  },
+  {
+    id: "first-npm-package",
+    timestamp: 1560173400, //2019
+    title: "clean-jsdoc-theme, My first NPM package",
+  },
+  {
+    id: "my-first-ml-project",
+    timestamp: 1591795800, // 2020
+    title: "Anu, ML model to predict protein-protein interactions.",
+  },
+  {
+    id: "cambridge",
+    timestamp: 1628559000, // August 10, 2021
+    title: "Joined University of Cambridge as Research Software Engineer.",
+  },
+  {
+    id: "startbahn",
+    timestamp: 1654824600, // June 10, 2022
+    title: "Landed on Japan, Joined Startbahn, Inc., as Frontend Engineer.",
+  },
+  {
+    id: "bodygram",
+    timestamp: 1688952600, // July 10, 2023
+    title: "Landed on Japan, Joined Startbahn, Inc., as Frontend Engineer.",
   },
 ];
