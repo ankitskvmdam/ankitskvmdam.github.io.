6124197127c58b6bc650c0899ecf2adbe58374ef
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-04-07 23:28:35 +0900

    make it netlify deployable

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths(), netlifyPlugin()],
 });
