c7cdb91ebce56ee99a5a5aa9d5feb68577c0c161
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-11-23 01:27:24 +0900

    chore: update logo style

--- app/components/logo/ank.tsx
+++ app/components/logo/ank.tsx
@@ -11,7 +11,11 @@ export function AnkLogo(props: TAnkLogoProps) {
 
   return (
     <svg
-      className={cn("size-8 fill-logo")}
+      className={cn(
+        "size-8 fill-accent-2 ank-logo",
+        showLoadingAnimation && "animation",
+        className,
+      )}
       viewBox="0 0 71.192383 72.131905"
       xmlns="http://www.w3.org/2000/svg"
       {...rest}
