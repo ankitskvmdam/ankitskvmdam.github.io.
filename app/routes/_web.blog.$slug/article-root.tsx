6011d6745f279d3c99744e86ed12491540c12559
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-11-27 16:44:10 +0900

    chore: add py to li element

--- app/routes/_web.blog.$slug/article-root.tsx
+++ app/routes/_web.blog.$slug/article-root.tsx
@@ -11,6 +11,7 @@ export function ArticleRoot(props: React.PropsWithChildren) {
         "[&_h2]:leading-normal [&_h2]:font-bold [&_h2]:text-2xl",
         "[&_h3]:leading-normal [&_h3]:font-bold [&_h3]:text-xl",
         "[&_ol]:list-decimal [&_ol]:pl-8 md:[&_ol]:pl-10",
+        "[&_li]:py-2",
         "[&_table]:w-full [&_table]:border-collapse [&_table]:my-13 [&_table]:text-sm",
         "[&_thead_td]:text-left [&_thead_td]:font-bold [&_thead_td]:px-4 [&_thead_td]:py-3 [&_thead_td]:border-b-0 [&_thead_td]:bg-muted/50",
         "[&_td]:px-4 [&_td]:py-3 [&_td]:border-b [&_td]:border-b-muted",
