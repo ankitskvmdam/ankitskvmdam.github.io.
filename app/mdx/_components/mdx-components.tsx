2f6661b84d71e9c88490086f1b568849eea7d98d
Ankit Kumar (अंकित कुमार) <ankits.kvmdam@hotmail.com>
2025-11-28 07:13:28 +0000

    feat: make h2 clickable

--- app/mdx/_components/mdx-components.tsx
+++ app/mdx/_components/mdx-components.tsx
@@ -1,6 +1,7 @@
 import { MDXComponents as TMDXComponents } from "mdx/types";
 import React from "react";
 import { cn } from "~/lib/utils";
+import { Link } from "lucide-react"
 
 export const MDXComponents: TMDXComponents = {
   img: ({ className, alt, ...rest }: React.HTMLProps<HTMLImageElement>) => (
@@ -16,16 +17,17 @@ export const MDXComponents: TMDXComponents = {
       <span className="text-xs">{alt}</span>
     </span>
   ),
-  h2: ({ children, ...rest }: React.HTMLProps<HTMLHeadingElement>) => (
-    <h2
-      id={
-        typeof children === "string"
-          ? children.toLowerCase().replace(/\s+/g, "-")
-          : ""
-      }
-      {...rest}
-    >
-      {children}
-    </h2>
-  ),
+  h2: ({ children, ...rest }: React.HTMLProps<HTMLHeadingElement>) => {
+    const id = typeof children === "string" ? children.toLowerCase().replace(/\s+/g, "-") : ""
+    return (
+      <h2
+        id={id}
+        {...rest}
+      >
+        <a className="relative group [&&]:text-current [&&]:no-underline" href={`#${id}`}>
+          <Link className="absolute left-0 top-2 -translate-x-6 opacity-0 group-hover:opacity-100 size-4" />
+        {children}
+        </a>
+      </h2>
+  )},
 };
