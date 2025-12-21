f95b25b89541aa5c218ae44ff70d4feda8cad708
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-12-21 22:44:05 +0900

    revert

--- app/routes/_web.project.mediapipe-webworker/preview-main.tsx
+++ app/routes/_web.project.mediapipe-webworker/preview-main.tsx
@@ -3,11 +3,6 @@ import { SelectInputDevice } from "~/layouts/select-input-video-device";
 import { PreviewViewParameters } from "./preview-view-parameters";
 import { PreviewUpdateParamters } from "./preview-update-parameters";
 import { PreviewRunner } from "./preview-runner";
-import { useSearchParams } from "react-router";
-import {
-  PREVIEW_TAB_SEARCH_PARAM,
-  TAB_SEARCH_PARAM_QUERY_NAME,
-} from "~/constants/search-params";
 
 export function PreviewMain() {
   const state = useStore((state) => ({
@@ -15,19 +10,14 @@ export function PreviewMain() {
     setInputVideoDevice: state.setInputVideoDevice,
   }));
 
-  const [searchParams] = useSearchParams();
-
   return (
     <div>
       <div className="flex justify-between gap-x-2">
         <div>
-          {searchParams.get(TAB_SEARCH_PARAM_QUERY_NAME) ===
-            PREVIEW_TAB_SEARCH_PARAM && (
-            <SelectInputDevice
-              inputVideoDevice={state.inputVideoDevice}
-              onValueChange={state.setInputVideoDevice}
-            />
-          )}
+          <SelectInputDevice
+            inputVideoDevice={state.inputVideoDevice}
+            onValueChange={state.setInputVideoDevice}
+          />
         </div>
         <PreviewViewParameters />
         <PreviewUpdateParamters />
