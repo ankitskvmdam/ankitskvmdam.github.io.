fc03d9b2d0090682903424c7fadc27fb9b7efc9e
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-12-28 03:15:48 +0900

    feat: add link component in mdx components

--- app/mdx/_components/mdx-components.tsx
+++ app/mdx/_components/mdx-components.tsx
@@ -1,7 +1,9 @@
 import { MDXComponents as TMDXComponents } from "mdx/types";
 import React from "react";
 import { cn } from "~/lib/utils";
-import { Link } from "lucide-react"
+import { LinkIcon } from "lucide-react";
+import { Link, TLinkProps } from "~/components/link";
+import * as routes from "~/constants/routes";
 
 export const MDXComponents: TMDXComponents = {
   img: ({ className, alt, ...rest }: React.HTMLProps<HTMLImageElement>) => (
@@ -18,16 +20,35 @@ export const MDXComponents: TMDXComponents = {
     </span>
   ),
   h2: ({ children, ...rest }: React.HTMLProps<HTMLHeadingElement>) => {
-    const id = typeof children === "string" ? children.toLowerCase().replace(/\s+/g, "-") : ""
+    const id =
+      typeof children === "string"
+        ? children.toLowerCase().replace(/\s+/g, "-")
+        : "";
     return (
-      <h2
-        id={id}
-        {...rest}
-      >
-        <a className="relative group [&&]:text-current [&&]:no-underline" href={`#${id}`}>
-          <Link className="absolute left-0 top-2 -translate-x-6 opacity-0 group-hover:opacity-100 size-4" />
-        {children}
+      <h2 id={id} {...rest}>
+        <a
+          className="relative group [&&]:text-current [&&]:no-underline"
+          href={`#${id}`}
+        >
+          <LinkIcon className="absolute left-0 top-2 -translate-x-6 opacity-0 group-hover:opacity-100 size-4" />
+          {children}
         </a>
       </h2>
-  )},
+    );
+  },
+  Link: ({ to, ...rest }: TLinkProps) => {
+    if (typeof to !== "string") {
+      console.error("[Link]: to should be a valid string");
+      return;
+    }
+
+    if (!(to in routes)) {
+      console.error(
+        `[Link]: to should be one of ${Object.keys(routes).join(", ")}`,
+      );
+      return null;
+    }
+
+    return <Link to={(routes as Record<string, string>)[to]} {...rest} />;
+  },
 };
