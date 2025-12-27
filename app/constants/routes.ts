e55788bc3d995a4db8d69665838ffbf9d7ba50f6
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-12-28 02:55:49 +0900

    fix: add tab query parameter in mediapipe-webworker url

--- app/constants/routes.ts
+++ app/constants/routes.ts
@@ -1,3 +1,5 @@
+import { PREVIEW_TAB_SEARCH_PARAM } from "./search-params";
+
 export const BASE_DEPLOYED_URL = "https://ankdev.me";
 
 export const PROJECTS_ROUTE = "/projects";
@@ -8,4 +10,4 @@ export const HOME_ROUTE = "/";
 
 // Project routes.
 export const PROJECT_ROUTE = "/project";
-export const MEDIAPIPE_WEBWORKER_ROUTE = `${PROJECT_ROUTE}/mediapipe-webworker`;
+export const MEDIAPIPE_WEBWORKER_ROUTE = `${PROJECT_ROUTE}/mediapipe-webworker?tab=${PREVIEW_TAB_SEARCH_PARAM}`;
